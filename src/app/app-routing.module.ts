c7e5aace9fb6513909d9085ea580f0e133364787
Meera Panchal <meera.panchal@yahoo.com>
2018-11-01 11:30:41 +0530

    user component error
    error resolved

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,7 +6,7 @@ import { PostsComponent } from './posts/posts.component';
 
 const routes: Routes = [
   {
-    path: 'reports',
+    path: '',
     component: UsersComponent
   },
   {
--- src/app/users/users.component.ts
+++ src/app/users/users.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { TableService } from './table/table.service';
+import { TableService } from '../table/table.service';
 
 @Component({
   selector: 'app-users',
